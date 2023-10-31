2e753f4d65ba3d96bd3965fec43e860bd1197a81
EbrahimTarek02 <ebrahimtarek2002@gmail.com>
2023-10-31 14:40:51 +0200

    Initial commit.

---
+++ lib/ui/util/app_assets.dart
@@ -0,0 +1,10 @@
+class AppAssets{
+  static const String imagePath = 'assets/images/';
+
+  static const String browseIcon = '${imagePath}browse_icon.svg';
+  static const String homeIcon = '${imagePath}home_icon.svg';
+  static const String noMoviesFoundIcon = '${imagePath}no_movies_found_icon.svg';
+  static const String searchIcon = '${imagePath}search_icon.svg';
+  static const String splash = '${imagePath}splash.svg';
+  static const String watchListIcon = '${imagePath}watch_list_icon.svg';
+}
---
+++ lib/ui/util/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color backgroundColor = Color(0xff121312);
+  static const Color primary = Color(0xff282A28);
+  static const Color accent = Color(0xffFFBB3B);
+  static const Color bottomNavBarBackgroundColor = Color(0xff1A1A1A);
+  static const Color appBarBackgroundColor = Color(0xff1D1E1D);
+  static const Color bottomNavBarUnSelectedItemColor = Color(0xffC6C6C6);
+  static const Color divider = Color(0xff707070);
+  static const Color white = Colors.white;
+}
